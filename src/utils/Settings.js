b3625e1bd376186213961c84987d5db9824de676
praveena-vuram <praveenamarya@vuram.com>
2021-02-01 12:36:37 +0530

    Initialize project

---
+++ src/utils/Settings.js
@@ -0,0 +1,13 @@
+export const RESOLUTION_ARR = {
+  "120p,120p_1": [160, 120, 15, 65],
+  "360p_4": [640, 360, 30, 600],
+  "480p_4": [640, 480, 30, 750],
+  "720p_3": [1280, 720, 30, 1710]
+};
+
+export const SHARE_ID = 1;
+
+export const APP_ID = "7797cc53df1a49368d57dc75c1d69cb8";
+
+// Assign Token to null if you have not enabled app certificate
+export const Token = "0067797cc53df1a49368d57dc75c1d69cb8IACVaAK9Q51g8xC5DD7xpQax0D+IApx5Tb5Ra/ZnycGUKRYw/4gAAAAAEABI+NBcMvMYYAEAAQAy8xhg";
